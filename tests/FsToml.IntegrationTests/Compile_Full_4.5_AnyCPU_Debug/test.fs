20a0b3145765f0c379fe59798d86f4a01b67042f
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2016-10-01 23:05:42 +0200

    Update test

--- tests/FsToml.IntegrationTests/Compile_Full_4.5_AnyCPU_Debug/test.fs
+++ tests/FsToml.IntegrationTests/Compile_Full_4.5_AnyCPU_Debug/test.fs
@@ -5,10 +5,4 @@ open System.Runtime.Versioning
 
 [<EntryPoint>]
 let main argv =
-    let asm = Assembly.GetEntryAssembly()
-    let att = asm.GetCustomAttributes(typedefof<TargetFrameworkAttribute>, false).[0] :?> TargetFrameworkAttribute
-    let name = att.FrameworkName
-
-    if name <> ".NETFramework,Version=v4.5" then
-        failwithf "Wrong Framework profile or version. Assembly version: %s" name
     0
