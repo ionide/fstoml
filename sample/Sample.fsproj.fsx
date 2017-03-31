ea300005afd3f5786b573ab9739a44c475d8fc5c
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2017-03-31 23:58:53 +0200

    Add TryParse methods

--- sample/Sample.fsproj.fsx
+++ sample/Sample.fsproj.fsx
@@ -3,7 +3,6 @@ System.IO.Directory.SetCurrentDirectory __SOURCE_DIRECTORY__
 #r "../bin/FsToml.fsproj/Forge.ProjectSystem.dll"
 #r "../bin/FsToml.fsproj/FsToml.dll"
 #r "../bin/FsToml.fsproj/FsToml.fsproj.dll"
-
 #r "System.Xml"
 #r "System.Xml.Linq"
 
@@ -13,4 +12,4 @@ open FsToml.ProjectSystem
 let t = FsToml.Parser.parse "testproject.toml"
 let target = { Target.BuildType = BuildType.Debug; PlatformType = PlatformType.AnyCPU; FrameworkTarget = FrameworkTarget.Net; FrameworkVersion = FrameworkVersion.V4_6}
 let p = FsToml.Transform.Fsproj.transform target t
-p
+p.ToXmlString()
